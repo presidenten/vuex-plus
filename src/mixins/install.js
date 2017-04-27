94b3a922bd4b134066741bb2a648887a4dec9b4b
Johan Hernefeldt <johans.inbox@gmail.com>
2017-04-27 23:39:09 +0200

    Vue & Vuex plugin as default export

--- src/mixins/install.js
+++ src/mixins/install.js
@@ -1,22 +1,8 @@
-import contextHmr from 'webpack-context-vuex-hmr';
-import { setup } from './addStore.js';
-import { hmrHandler, setStore } from '../common/hmrHandler.js';
-
-let setupDone = false;
 export default {
   install(Vue) {
     Vue.mixin({
       props: ['instance'],
       created() {
-        if (!setupDone && this.$store) {
-          setStore(this.$store);
-          const importer = contextHmr.getNewInstance();
-          setup(importer);
-          importer.getModules();
-          importer.setupHMR(hmrHandler);
-          setupDone = true;
-        }
-
         const findModuleName = (parent) => {
           if (!this['$vuex+'] && parent.$parent) {
             if (!parent.$parent['$vuex+']) {
