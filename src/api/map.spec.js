4beb1c55ef5b25b054e9365f8d0ac4c94335d3dc
Johan Hernefeldt <johans.inbox@gmail.com>
2017-05-07 18:20:53 +0200

    Tests prepared for jest 20

--- src/api/map.spec.js
+++ src/api/map.spec.js
@@ -3,6 +3,7 @@ import * as api from './api.js';
 
 let store;
 let subapi;
+let self;
 
 beforeEach(() => {
   store = {
@@ -25,116 +26,87 @@ beforeEach(() => {
     },
   };
 
+  self = {
+    $store: store,
+    '$vuex+': {
+      baseStoreName: 'top',
+      storeInstanceName: 'top',
+    },
+  };
   api.setApi({ top: subapi, top$foo: api.remapBaseStore(subapi, 'top', 'top$foo') });
 });
 
 describe('map.getters', () => {
-  it('should return global getters', function() { // eslint-disable-line
-    this['$vuex+'] = {
-      baseStoreName: 'top',
-      storeInstanceName: 'top',
-    };
-    this.$store = store;
-    const getters = map.getters.call(this, {
+  it('should return global getters', () => {
+    const getters = map.getters.call(self, {
       path: 'top/subtree/path',
     });
-    expect(getters.path.call(this)).toEqual(store.getters['top/subtree/path']);
+    expect(getters.path.call(self)).toEqual(store.getters['top/subtree/path']);
   });
 
-  it('should return global getters with instances', function() { // eslint-disable-line
-    this['$vuex+'] = {
-      baseStoreName: 'top',
-      storeInstanceName: 'top$foo',
-    };
-    this.instance = 'bar';
-    this.$store = store;
-    const getters = map.getters.call(this, {
+  it('should return global getters with instances', () => {
+    self['$vuex+'].storeInstanceName = 'top$foo';
+    self.instance = 'bar';
+
+    const getters = map.getters.call(self, {
       path: 'top$foo/subtree$bar/path',
     });
-    expect(getters.path.call(this)).toEqual(store.getters['top$foo/subtree$bar/path']);
+    expect(getters.path.call(self)).toEqual(store.getters['top$foo/subtree$bar/path']);
   });
 
-  it('should return local getters', function() { // eslint-disable-line
-    this['$vuex+'] = {
-      baseStoreName: 'top',
-      storeInstanceName: 'top',
-    };
-    this.$store = store;
-    const getters = map.getters.call(this, {
+  it('should return local getters', () => {
+    const getters = map.getters.call(self, {
       path: 'subtree/path',
     });
-    expect(getters.path.call(this)).toEqual(store.getters['top/subtree/path']);
+    expect(getters.path.call(self)).toEqual(store.getters['top/subtree/path']);
   });
 
-  it('should return local getters with instances', function() { // eslint-disable-line
-    this['$vuex+'] = {
-      baseStoreName: 'top',
-      storeInstanceName: 'top',
-    };
-    this.$store = store;
-    this.instance = 'bar';
-    const getters = map.getters.call(this, {
+  it('should return local getters with instances', () => {
+    self.instance = 'bar';
+    const getters = map.getters.call(self, {
       path: 'subtree/path',
     });
-    expect(getters.path.call(this)).toEqual(store.getters['top/subtree$bar/path']);
+    expect(getters.path.call(self)).toEqual(store.getters['top/subtree$bar/path']);
   });
 });
 
 describe('map.actions', () => {
-  it('should return global actions', function() { // eslint-disable-line
-    this['$vuex+'] = {
-      baseStoreName: 'top',
-      storeInstanceName: 'top',
-    };
-    this.$store = store;
-    const actions = map.actions.call(this, {
+  it('should return global actions', () => {
+    const actions = map.actions.call(self, {
       path: 'top/subtree/path',
     });
-    actions.path.call(this, 13);
+    actions.path.call(self, 13);
     expect(store.dispatch).toBeCalledWith('top/subtree/path', 13);
   });
 
   it('should return global actions with instances', function() { // eslint-disable-line
-    this['$vuex+'] = {
-      baseStoreName: 'top',
-      storeInstanceName: 'top$foo',
-    };
-    this.instance = 'bar';
-    this.$store = store;
-    const actions = map.actions.call(this, {
+    self['$vuex+'].storeInstanceName = 'top$foo';
+    self.instance = 'bar';
+    const actions = map.actions.call(self, {
       path: 'top$foo/subtree$bar/path',
     });
-    actions.path.call(this, 42);
+    actions.path.call(self, 42);
     expect(store.dispatch).toBeCalledWith('top$foo/subtree$bar/path', 42);
   });
 
   it('should return local actions', function() { // eslint-disable-line
-    this['$vuex+'] = {
-      baseStoreName: 'top',
-      storeInstanceName: 'top',
-    };
-    this.$store = store;
-    const actions = map.actions.call(this, {
+    const actions = map.actions.call(self, {
       path: 'subtree/path',
     });
-    actions.path.call(this, 19);
+    actions.path.call(self, 19);
     expect(store.dispatch).toBeCalledWith('top/subtree/path', 19);
   });
 
   it('should return local actions with instances', function() { // eslint-disable-line
-    this['$vuex+'] = {
-      baseStoreName: 'top',
-      storeInstanceName: 'top$foo',
-    };
-    this.$store = store;
-    this.instance = 'bar';
-    this.$parent = {
+    self['$vuex+'].storeInstanceName = 'top$foo';
+    self.instance = 'bar';
+    self.$parent = {
       instance: 'foo',
     };
-    const actions = map.actions.call(this, {
+    const actions = map.actions.call(self, {
       path: 'subtree/path',
     });
-    actions.path.call(this, 99);
+    actions.path.call(self, 99);
     expect(store.dispatch).toBeCalledWith('top$foo/subtree$bar/path', 99);
   });
 });
